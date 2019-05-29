3f19dbe2f6d240a9d70636ef4db2ca322af292c0
Lukas Siemon <github@blackflux.com>
2019-05-29 09:58:31 -0700

    amend: coverage fix

--- test/util/plugin.spec.js
+++ test/util/plugin.spec.js
@@ -2,11 +2,17 @@ const { expect } = require('chai');
 const { injectPlugin, filterPlugin, sortPlugin } = require('../../src/util/plugin');
 
 describe('Testing plugin', () => {
+  const fn = () => null;
+
+  it('Testing fn', () => {
+    expect(fn()).to.equal(null);
+  });
+
   it('Testing nested prefix', () => {
     const result = filterPlugin({
       target: 'key',
       requires: ['req'],
-      fn: () => {}
+      fn
     })('prefix');
     expect(result.target).to.equal('prefix.key');
     expect(result.requires).to.deep.equal(['prefix.req']);
@@ -16,7 +22,7 @@ describe('Testing plugin', () => {
     const result = filterPlugin({
       target: 'key',
       requires: ['req'],
-      fn: () => {}
+      fn
     })('');
     expect(result.target).to.equal('key');
     expect(result.requires).to.deep.equal(['req']);
@@ -26,7 +32,7 @@ describe('Testing plugin', () => {
     const result = filterPlugin({
       target: '*',
       requires: ['*'],
-      fn: () => {}
+      fn
     })('prefix');
     expect(result.target).to.equal('prefix.');
     expect(result.requires).to.deep.equal(['prefix.']);
@@ -36,16 +42,16 @@ describe('Testing plugin', () => {
     const result = filterPlugin({
       target: '*',
       requires: ['*'],
-      fn: () => {}
+      fn
     })('');
     expect(result.target).to.equal('');
     expect(result.requires).to.deep.equal(['']);
   });
 
   it('Testing plugin types', () => {
-    const resultInject = injectPlugin({ target: '*', requires: [], fn: () => {} })('');
-    const resultFilter = filterPlugin({ target: '*', requires: [], fn: () => {} })('');
-    const resultSort = sortPlugin({ target: '*', requires: [], fn: () => {} })('');
+    const resultInject = injectPlugin({ target: '*', requires: [], fn })('');
+    const resultFilter = filterPlugin({ target: '*', requires: [], fn })('');
+    const resultSort = sortPlugin({ target: '*', requires: [], fn })('');
     expect(resultInject.type).to.equal('INJECT');
     expect(resultFilter.type).to.equal('FILTER');
     expect(resultSort.type).to.equal('SORT');
