48a0a849e24b2af399fb6ff4ed4c26d3980110e5
Lukas Siemon <github@blackflux.com>
2019-09-11 15:13:08 -0700

    amend: added more test cases

--- test/util/rewriter.spec.js
+++ test/util/rewriter.spec.js
@@ -116,7 +116,7 @@ describe('Testing rewriter', () => {
     expect(data).to.deep.equal([{ id: 1 }, { id: 2 }]);
   });
 
-  it('Testing sort primitives', () => {
+  it('Testing sort list primitive', () => {
     const dataStoreFields = ['numbers'];
     const data = [{ numbers: [2, 1] }];
     const fields = ['numbers'];
@@ -131,6 +131,40 @@ describe('Testing rewriter', () => {
     expect(rew.fieldsToRequest).to.deep.equal(fields);
     rew.rewrite(data);
     expect(data).to.deep.equal([{ numbers: [1, 2] }]);
+  });
+
+  it('Testing filter list primitive', () => {
+    const dataStoreFields = ['numbers'];
+    const data = [{ numbers: [2, 1] }];
+    const fields = ['numbers'];
+    const plugin = filterPlugin({
+      target: '*',
+      requires: [],
+      fn: ({ value }) => value === 2
+    });
+    const rew = rewriter({
+      numbers: [plugin]
+    }, dataStoreFields).init(fields);
+    expect(rew.fieldsToRequest).to.deep.equal(fields);
+    rew.rewrite(data);
+    expect(data).to.deep.equal([{ numbers: [2] }]);
+  });
+
+  it('Testing filter non-list primitive', () => {
+    const dataStoreFields = ['numbers'];
+    const data = [{ numbers: 2 }];
+    const fields = ['numbers'];
+    const plugin = filterPlugin({
+      target: '*',
+      requires: [],
+      fn: ({ value }) => value === 1
+    });
+    const rew = rewriter({
+      numbers: [plugin]
+    }, dataStoreFields).init(fields);
+    expect(rew.fieldsToRequest).to.deep.equal(fields);
+    rew.rewrite(data);
+    expect(data).to.deep.equal([{}]);
   });
 
   it('Testing multiple sort plugins', () => {
