9aaba78fd8722e28ab97e2fe941937625e654d02
Lukas Siemon <github@blackflux.com>
2020-12-08 14:19:55 -0800

    chore: coverage

--- test/util/validation-extract-keys.spec.js
+++ test/util/validation-extract-keys.spec.js
@@ -2,33 +2,42 @@ const { expect } = require('chai');
 const validationExtractKeys = require('../../src/util/validation-extract-keys');
 
 describe('Testing validation-extract-keys.js', () => {
+  let fn;
+  before(() => {
+    fn = (e) => typeof e === 'string';
+  });
+
+  it('Testing fn', () => {
+    expect(fn('str')).to.equal(true);
+  });
+
   it('Testing function', () => {
-    const keys = validationExtractKeys('prefix', (e) => typeof e === 'string');
+    const keys = validationExtractKeys('prefix', fn);
     expect(keys).to.deep.equal(['prefix']);
   });
 
   it('Testing array', () => {
-    const keys = validationExtractKeys('prefix', [(e) => typeof e === 'string']);
+    const keys = validationExtractKeys('prefix', [fn]);
     expect(keys).to.deep.equal(['prefix']);
   });
 
   it('Testing object', () => {
-    const keys = validationExtractKeys('prefix', { key: (e) => typeof e === 'string' });
+    const keys = validationExtractKeys('prefix', { key: fn });
     expect(keys).to.deep.equal(['prefix.key']);
   });
 
   it('Testing object with empty string key', () => {
-    const keys = validationExtractKeys('prefix', { '': (e) => typeof e === 'string' });
+    const keys = validationExtractKeys('prefix', { '': fn });
     expect(keys).to.deep.equal(['prefix.']);
   });
 
   it('Testing object with empty prefix', () => {
-    const keys = validationExtractKeys('', { key: (e) => typeof e === 'string' });
+    const keys = validationExtractKeys('', { key: fn });
     expect(keys).to.deep.equal(['.key']);
   });
 
   it('Testing object with empty string key with empty prefix', () => {
-    const keys = validationExtractKeys('', { '': (e) => typeof e === 'string' });
+    const keys = validationExtractKeys('', { '': fn });
     expect(keys).to.deep.equal(['.']);
   });
 });
