ac0a687d7786d24a7441abea6a072ad5c04d8bc7
Lukas Siemon <lukas.siemon@blackflux.com>
2022-01-25 13:42:34 -0800

    amend: minor

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,5 +1,5 @@
 import { expect } from 'chai';
-import index from '../src/index.js';
+import * as index from '../src/index.js';
 
 describe('Testing Package', () => {
   it('Testing Exposed', () => {
